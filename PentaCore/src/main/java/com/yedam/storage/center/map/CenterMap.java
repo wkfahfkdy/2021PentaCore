45d59940c4f7653cc10c303f1e42dedf6fe117e0
DAM <DAM@DESKTOP-HLUJG4C>
2021-07-29 17:53:29 +0900

    dam - 1:1문의 진행중

--- PentaCore/src/main/java/com/yedam/storage/center/map/CenterMap.java
+++ PentaCore/src/main/java/com/yedam/storage/center/map/CenterMap.java
@@ -3,16 +3,23 @@ package com.yedam.storage.center.map;
 import java.util.List;
 
 import com.yedam.storage.center.vo.CenterVO;
+import com.yedam.storage.paging.paging;
 
 public interface CenterMap {
-	
-	// FAQ
+
+	//FAQ
 	public List<CenterVO> faqSelectList();
 	
-	// 1:1
-	public List<CenterVO> inquirySelectList();
-	public int inquiryInsert();
-	public int inquiryDelete();
+	//1:1
+	public List<paging> inquirySelectList();
+	public List<paging> inquiryPaging(CenterVO vo);
+	public int inquiryInsert(CenterVO vo);
+	public int inquiryDelete();	
+	
+	//paging
+	public int tableCnt();
+	
+	//댓글
 	public List<CenterVO> replyList();
 	public int replyInsert();
 	public int replyDelete();
--- PentaCore/src/main/java/com/yedam/storage/paging/paging.java
+++ PentaCore/src/main/java/com/yedam/storage/paging/paging.java
@@ -1,8 +1,10 @@
 package com.yedam.storage.paging;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
 public class paging {
 	private int pageSize;
 	private int firstPageNo;
@@ -14,7 +16,81 @@ public class paging {
 	private int finalPageNo;
 	private int totalCount;
 	
-	 private void makePaging() {
+	 public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getFirstPageNo() {
+		return firstPageNo;
+	}
+
+	public void setFirstPageNo(int firstPageNo) {
+		this.firstPageNo = firstPageNo;
+	}
+
+	public int getPrevPageNo() {
+		return prevPageNo;
+	}
+
+	public void setPrevPageNo(int prevPageNo) {
+		this.prevPageNo = prevPageNo;
+	}
+
+	public int getStartPageNo() {
+		return startPageNo;
+	}
+
+	public void setStartPageNo(int startPageNo) {
+		this.startPageNo = startPageNo;
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getEndPageNo() {
+		return endPageNo;
+	}
+
+	public void setEndPageNo(int endPageNo) {
+		this.endPageNo = endPageNo;
+	}
+
+	public int getNextPageNo() {
+		return nextPageNo;
+	}
+
+	public void setNextPageNo(int nextPageNo) {
+		this.nextPageNo = nextPageNo;
+	}
+
+	public int getFinalPageNo() {
+		return finalPageNo;
+	}
+
+	public void setFinalPageNo(int finalPageNo) {
+		this.finalPageNo = finalPageNo;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+		this.makePaging();
+	}
+	
+
+	private void makePaging() {
 	      if (this.totalCount == 0) return; // 게시 글 전체 수가 없는 경우
 	      if (this.pageNo == 0) this.setPageNo(1); // 기본 값 설정
 	      if (this.pageSize == 0) this.setPageSize(10); // 기본 값 설정
